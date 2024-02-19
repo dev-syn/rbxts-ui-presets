25e6b9d434109ca9cf3a5c2a0ecbd2093eb11d3a
Casey <103451081+dev-syn@users.noreply.github.com>
2024-02-19 15:18:37 -0500

    Switch math operation protocol

--- src/components/ContextMenu.ts
+++ src/components/ContextMenu.ts
@@ -126,7 +126,7 @@ class ContextMenu {
         if (this.viewSize) {
             
             if (centerTopPos < 0)
-                this.MenuBG.Position = this.MenuBG.Position.sub(new UDim2(0,0,0,centerTopPos + absSizeY / 2));
+                this.MenuBG.Position = this.MenuBG.Position.sub(new UDim2(0,0,0,centerTopPos - absSizeY / 2));
             if (centerBottomPos > this.viewSize.Y)
                 this.MenuBG.Position = this.MenuBG.Position.add(new UDim2(0,0,0,this.viewSize.Y - centerBottomPos - absSizeY / 2));
 
