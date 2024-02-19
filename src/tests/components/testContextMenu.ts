0d5ada7080a6689e7b8765913e04bb3c1872fd3a
Casey <103451081+dev-syn@users.noreply.github.com>
2024-02-19 13:32:10 -0500

    Add thread stay alive

--- src/tests/components/testContextMenu.ts
+++ src/tests/components/testContextMenu.ts
@@ -15,3 +15,8 @@ btn.Parent = sg;
 
 const menu: ContextMenu = new ContextMenu(btn);
 menu.AddContext(new ContextItem("Say Hello",() => print("Hello!")));
+
+while(true) {
+    print("Still running: " + menu);
+    task.wait(2);
+}
