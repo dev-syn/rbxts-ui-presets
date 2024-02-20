f81aeec0d3e9029cd96a1113339c2991e023163e
Casey <103451081+dev-syn@users.noreply.github.com>
2024-02-20 13:49:14 -0500

    Export only the static classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
+import { ContextMenu } from './components/ContextMenu';
 import { SelectableGroup } from './components/SelectableGroup';
 
-/**
- * This module contains references to all the UIPresets modules.
- */
-class UIPresets {
-	/**
-	 * The SelectableGroup class reference.
-	 */
-	static SelectableGroup: typeof SelectableGroup = SelectableGroup;
-};
-
-export = UIPresets;
+export { ContextMenu, SelectableGroup };
