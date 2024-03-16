d198f56d829e62f966f7ffdeb82b48878d3fc7ca
Casey <103451081+dev-syn@users.noreply.github.com>
2024-03-16 10:16:09 -0400

    Add component exports to entry module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { ContextMenu } from './components/ContextMenu';
-import { SelectableGroup } from './components/SelectableGroup';
-import Navbar from './components/Navbar';
+import { UIPresetsConfig } from './UIPresetsConfig';
 
-export { ContextMenu, SelectableGroup, Navbar };
+import { ContextMenu } from './components/ContextMenu';
+import Navbar from './components/Navbar';
+import { SelectableGroup } from './components/SelectableGroup';
+import { ToolTip } from './components/ToolTip';
+import { BoundCheck, BoundsLayout } from './components/BoundCheck';
+
+export { UIPresetsConfig, ContextMenu, Navbar, SelectableGroup, ToolTip, BoundCheck, BoundsLayout };
