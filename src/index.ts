7458278e14b98ca9df34faae15250154562b463e
Casey <103451081+dev-syn@users.noreply.github.com>
2024-10-07 08:53:50 -0400

    Add component & preset exports

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,14 @@ import Navbar from './components/Navbar';
 import { SelectableGroup } from './components/SelectableGroup';
 import { ToolTip } from './components/ToolTip';
 import { BoundCheck, BoundsLayout } from './components/BoundCheck';
+import { TickSetting } from 'presets/settings/TickSetting';
+import { CloseBtn } from 'presets/buttons/CloseBtn';
+import { SettingsCreator } from 'components/SettingsCreator';
 
-export { UIPresetsConfig, ContextMenu, Navbar, SelectableGroup, ToolTip, BoundCheck, BoundsLayout };
+export { 
+    UIPresetsConfig,
+    // Components
+    SettingsCreator, BoundCheck, BoundsLayout, ContextMenu, Navbar, SelectableGroup, ToolTip,
+    // Presets
+    CloseBtn, TickSetting
+};
