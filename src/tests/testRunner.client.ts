c8f8242321514c0a21d4d4eb233a961bac283743
Casey <103451081+dev-syn@users.noreply.github.com>
2025-07-19 15:57:34 -0400

    Add test runner for ui-presets

--- src/tests/testRunner.client.ts
+++ src/tests/testRunner.client.ts
@@ -1,1 +1,14 @@
 // Simulate flamework
+import { Flamework } from '@flamework/core';
+import { discoverFlameworkPackages } from '@rbxts/syn-utils/out/flamework.utilities';
+import { resolveInstance } from '@rbxts/syn-utils/out/path.utilities';
+
+const container: Folder | undefined = resolveInstance("ReplicatedStorage/rbxts_include/node_modules","Folder");
+if (container) {
+	// Load all other packages into flamework
+	const discoveredPkgs: string[] = discoverFlameworkPackages(container);
+	discoveredPkgs.forEach(path => Flamework.addPaths(path));
+	// Load ui-presets into flamework
+	Flamework.addPaths("ReplicatedStorage/ui-presets");
+	Flamework.ignite();
+} else warn("[ui-presets] -> {testRunner}: Package container could not be found, so nothing was ignited.");
