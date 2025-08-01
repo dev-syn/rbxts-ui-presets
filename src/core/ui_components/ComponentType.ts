e2a56d6a9bdd6c6aa478cbee15086ffb1c97b194
Casey <103451081+dev-syn@users.noreply.github.com>
2025-08-01 15:11:37 -0400

    Delete in favor of ComponentTag

--- src/core/ui_components/ComponentType.ts
+++
@@ -1,8 +0,0 @@
-enum ComponentType {
-	BoundCheck = 'uipres_component_boundcheck',
-	ContextMenu = 'uipres_component_contextmenu',
-	SelectableGroup = 'uipres_component_selectablegroup',
-	ToolTip = 'uipres_component_tooltip'
-}
-
-export = ComponentType;
