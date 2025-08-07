53afe1a0625c0e12805db9bfb65e81c1d5f30262
Casey <103451081+dev-syn@users.noreply.github.com>
2025-08-07 16:58:50 -0400

    Move ContextMenu & ContextItem to presets

--- src/core/ui_components/ComponentRegistry.ts
+++ src/core/ui_components/ComponentRegistry.ts
@@ -1,19 +1,16 @@
 import { BoundCheck } from './.comps/BoundCheck'
-import { ContextMenu } from './.comps/ContextMenu'
 import { SelectableGroup } from './.comps/SelectableGroup'
 import { ToolTip } from './.comps/ToolTip'
 import { ComponentTag } from './ComponentTag'
 
 type TComponentRegistry = {
 	[ComponentTag.BoundCheck]: typeof BoundCheck,
-	[ComponentTag.ContextMenu]: typeof ContextMenu,
 	[ComponentTag.SelectableGroup]: typeof SelectableGroup,
 	[ComponentTag.ToolTip]: typeof ToolTip;
 }
 
 const ComponentRegistry: TComponentRegistry = {
 	[ComponentTag.BoundCheck]: BoundCheck,
-	[ComponentTag.ContextMenu]: ContextMenu,
 	[ComponentTag.SelectableGroup]: SelectableGroup,
 	[ComponentTag.ToolTip]: ToolTip
 } as const;
