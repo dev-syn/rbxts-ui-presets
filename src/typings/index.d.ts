430336ef208b18c79ed813b0ab05e3144ce62422
Casey <103451081+dev-syn@users.noreply.github.com>
2025-08-07 16:59:07 -0400

    Add a type that represents both component & preset ID tags

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,10 +1,14 @@
 import { TPresetRegistry } from '../core/presets/PresetRegistry'
+import { PresetTag } from '../core/presets/PresetTag'
 import { TComponentRegistry } from '../core/ui_components/ComponentRegistry'
+import { ComponentTag } from '../core/ui_components/ComponentTag'
 
 export type UUID = string
 
 export type UIPresetsPresetType = keyof TPresetRegistry
 export type UIPresetsComponentType = keyof TComponentRegistry
+
+export type UIPresetComponents = keyof typeof PresetTag & keyof typeof ComponentTag;
 
 export type FW_Attributes = { [key: string]: unknown }
 
